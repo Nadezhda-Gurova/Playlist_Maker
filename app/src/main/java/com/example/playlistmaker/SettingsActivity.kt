2a484692d53a8c940ee572381a8ca5797264e25c
Nadezhda-Gurova <yudinanade@yandex.ru>
2023-04-29 17:30:52 +0200

    Реализовала экран настроек полностью: _ Реализовать кнопку «Поделиться приложением» - Реализовать кнопку «Написать в поддержку» - Реализовать кнопку «Пользовательское соглашение»

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,11 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.Intent.createChooser
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +17,40 @@ class SettingsActivity : AppCompatActivity() {
         back.setOnClickListener {
             finish()
         }
+
+        val search = findViewById<ImageView>(R.id.share_ic)
+
+        search.setOnClickListener {
+            Intent(Intent.ACTION_SEND).apply {
+                val shareMessage = "https://practicum.yandex.ru/android-developer/"
+                putExtra(Intent.EXTRA_TEXT, shareMessage)
+                type = "text/plain"
+                startActivity(createChooser(this, ""))
+            }
+        }
+
+        val writeToSupport = findViewById<ImageView>(R.id.write_to_support_ic)
+        writeToSupport.setOnClickListener {
+            Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("nyud91@gmail.com"))
+                putExtra(
+                    Intent.EXTRA_SUBJECT,
+                    "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+                )
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    "Спасибо разработчикам и разработчицам за крутое приложение!"
+                )
+                startActivity(this)
+            }
+        }
+
+        val termsOfUse = findViewById<ImageView>(R.id.terms_of_use_ic)
+        termsOfUse.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW,  Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            startActivity(createChooser(intent, "Choose Application"))
+        }
     }
+
 }
