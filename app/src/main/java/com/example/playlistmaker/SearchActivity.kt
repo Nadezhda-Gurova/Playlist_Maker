30dda34f0ef80f18fc9e771d53d0f68fd20a7497
Nadezhda-Gurova <yudinanade@yandex.ru>
2023-05-12 17:40:40 +0200

    дописала код в onSaveInstanceState, onRestoreInstanceState

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,23 +1,33 @@
 package com.example.playlistmaker
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
-import android.widget.LinearLayout
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.appcompat.app.AppCompatActivity
 
+const val log = "# SearchActivity"
 class SearchActivity : AppCompatActivity() {
+    companion object {
+        const val SEARCH_INPUT = "SEARCH_INPUT"
+    }
+
+    init {
+        Log.d(log, "constructor")
+    }
+
+    private lateinit var inputEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(log, "onCreate")
         setContentView(R.layout.activity_search)
 
 //        val linearLayout = findViewById<ConstraintLayout>(R.id.container)
-        val inputEditText = findViewById<EditText>(R.id.inputEditText)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        inputEditText = findViewById(R.id.inputEditText)
 
         clearButton.setOnClickListener {
             inputEditText.setText("")
@@ -51,4 +61,42 @@ class SearchActivity : AppCompatActivity() {
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(log, "onSaveInstanceState")
+        outState.putString(SEARCH_INPUT, inputEditText.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d(log, "onRestoreInstanceState")
+        inputEditText.setText(savedInstanceState.getString(SEARCH_INPUT, ""))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(log, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(log, "onPause")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(log, "onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(log, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(log, "onDestroy")
+    }
+
 }
